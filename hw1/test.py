39a2863c59522e87d058a602f2804cee0faadc1c
NewGamezzz <folk48560@gmail.com>
2023-01-12 20:15:06 +0700

    add homework 1 and 2

---
+++ hw1/test.py
@@ -0,0 +1,13 @@
+import gym
+
+env = gym.make("Ant-v4", render_mode="human")
+
+observation = env.reset(seed=42)
+for _ in range(1000):
+    env.render()
+    action = env.action_space.sample() # User-defined policy function
+    observation, reward, terminated, info = env.step(action)
+
+    if terminated:
+        observation = env.reset()
+env.close()
